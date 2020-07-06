2782ae1ed082795621bbbe268c57176bac7c0b2b
neeraj-d2 <67588050+neeraj-d2@users.noreply.github.com>
2020-07-06 23:32:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World from neeraj version v1');
+  res.send('Hello World from neeraj version v2');
 });
 
 app.listen(PORT, HOST);
